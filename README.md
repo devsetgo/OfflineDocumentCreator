e1563d82f9f56a7bf3dee0ba5cbab50c8549e197
devsetgo <mikeryan56@gmail.com>
2023-11-18 03:53:41 +0000

    fixes and adding unsync

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OfflineDocumentCreator
 Creating a way to make offline documentation from a few of my favorite libraries.
+
+### Libraries setup to Download
+The libraries listed - FastAPI, SQLAlchemy, Loguru, HTTPX, TQDM, Pydantic, Textblob, Pytest, and Passlib - are the ones that will be downloaded. Each library's documentation will be fetched from its respective website, converted into a PDF, and then all these PDFs will be combined into a single document. This consolidated PDF will provide a comprehensive reference for all these libraries, making it easier to access and navigate through their documentation.
+
+More to be added...
--- src/main.py
+++ src/main.py
@@ -5,11 +5,13 @@ import os
 from urllib.parse import urlparse, urljoin
 from tqdm import tqdm  # For progress bar
 import shutil
+from unsync import unsync
 
 failed_urls = []
 good_urls = []
 
 
+@unsync
 def get_all_links(data_dict: dict):
     # Create a directory to store the PDFs
     if not os.path.exists("documents"):
@@ -23,7 +25,7 @@ def get_all_links(data_dict: dict):
     soup = BeautifulSoup(response.text, "html.parser")
     links = soup.find_all("a")
 
-    for link in tqdm(links, leave=False, ascii=True, desc=data_dict["name"]):
+    for link in tqdm(links, leave=True, ascii=True, desc=data_dict["name"],unit="pages"):
         url = link.get("href")
         if url:
             full_url = urljoin(base_url, url)
@@ -42,9 +44,27 @@ def get_all_links(data_dict: dict):
                         f'documents/{domain}_{urlparse(full_url).path.replace("/", "_")}.pdf',
                     )
                     good_urls.append(full_url)
+                    
                 except Exception as e:
                     print(f"Failed to create PDF for {full_url}. Reason: {str(e)}")
                     failed_urls.append(full_url)
+    return data_dict["name"]
+
+@unsync
+def download_pdf(library):
+    r = requests.get(library["base_url"], stream=True)
+    if r.status_code == 200:
+        total_size_in_bytes= int(r.headers.get('content-length', 0))
+        block_size = 1024 #1 Kibibyte
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True,leave=True, ascii=True, desc=library["name"])
+        with open(f'documents/{str(library["name"]).lower()}.pdf', 'wb') as f:
+            for data in r.iter_content(block_size):
+                progress_bar.update(len(data))
+                f.write(data)
+        progress_bar.close()
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print("ERROR, something went wrong")
+    return "done"
 
 
 def merge_pdfs():
@@ -54,7 +74,7 @@ def merge_pdfs():
     pdfs = glob.glob("documents/*.pdf")
     merger = PdfMerger()
 
-    for pdf in tqdm(pdfs, ascii=True, leave=True):
+    for pdf in tqdm(pdfs, ascii=False, leave=True):
         # print(pdf)
         merger.append(pdf)
 
@@ -65,39 +85,39 @@ def merge_pdfs():
 
 def main():
     library_list: list = [
-        {
-            "name": "FastAPI",
-            "base_url": "https://fastapi.tiangolo.com",
-            "hrefs_to_skip": [
-                "/sponsors/",
-                "/de/",
-                "/em/",
-                "/es/",
-                "/fa/",
-                "/fr/",
-                "/he/",
-                "/id/",
-                "/ja/",
-                "/ko/",
-                "/pl/",
-                "/pt/",
-                "/ru/",
-                "/tr/",
-                "/uk/",
-                "/ur/",
-                "/vi/",
-                "/yo/",
-                "/zh/",
-                "/zh/",
-            ],
-            "pdf": False,
-        },
-        {
-            "name": "SQLAlchemy",
-            "base_url": "https://docs.sqlalchemy.org/en/20/",
-            "hrefs_to_skip": [".zip"],
-            "pdf": False,
-        },
+        # {
+        #     "name": "FastAPI",
+        #     "base_url": "https://fastapi.tiangolo.com",
+        #     "hrefs_to_skip": [
+        #         "/sponsors/",
+        #         "/de/",
+        #         "/em/",
+        #         "/es/",
+        #         "/fa/",
+        #         "/fr/",
+        #         "/he/",
+        #         "/id/",
+        #         "/ja/",
+        #         "/ko/",
+        #         "/pl/",
+        #         "/pt/",
+        #         "/ru/",
+        #         "/tr/",
+        #         "/uk/",
+        #         "/ur/",
+        #         "/vi/",
+        #         "/yo/",
+        #         "/zh/",
+        #         "/zh/",
+        #     ],
+        #     "pdf": False,
+        # },
+        # {
+        #     "name": "SQLAlchemy",
+        #     "base_url": "https://docs.sqlalchemy.org/en/20/",
+        #     "hrefs_to_skip": [".zip"],
+        #     "pdf": False,
+        # },
         {
             "name": "Loguru",
             "base_url": "https://loguru.readthedocs.io/en/stable/",
@@ -146,34 +166,26 @@ def main():
             "hrefs_to_skip": [],
             "pdf": True,
         },
+        {
+            "name": "Passlib",
+            "base_url": "https://passlib.readthedocs.io/_/downloads/en/stable/pdf/",
+            "hrefs_to_skip": [],
+            "pdf": True,
+        },
         # {
-        #     "name": "Passlib",
-        #     "base_url": "https://passlib.readthedocs.io/_/downloads/en/stable/pdf/",
-        #     "hrefs_to_skip": [],
-        #     "pdf": True,
-        # },
-        # {
-        #     "name": "Xyz",
+        #     "name": "Sample",
         #     "base_url": "https://xyz.com",
         #     "hrefs_to_skip": [],
         #     "pdf": False,
         # },
     ]
-    for library in tqdm(library_list, ascii=True, leave=True):
-        if library["pdf"] is True:
-            r = requests.get(library["base_url"], stream=True)
-            if r.status_code == 200:
-                with open(
-                    f'documents/{str(library["name"]).lower()}.pdf',
-                    "wb",
-                ) as f:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, f)
-        else:
-            get_all_links(data_dict=library)
-            # pass
+    tasks = [download_pdf(library) if library["pdf"] is True else get_all_links(data_dict=library) \
+        for library in tqdm(library_list, ascii=False, leave=True, desc="Libraries Processing")]
+    
+    for task in tasks:
+        task.result()
 
-    merge_pdfs()
+    # merge_pdfs()
 
 
 if __name__ == "__main__":
