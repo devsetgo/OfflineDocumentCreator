51057f15feec038f98020c7975e0db0dd33641c9
devsetgo <mikeryan56@gmail.com>
2023-11-18 03:00:27 +0000

    adding ability to direct download if a PDF.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import pdfkit
 import os
 from urllib.parse import urlparse, urljoin
 from tqdm import tqdm  # For progress bar
+import shutil
 
 failed_urls = []
 good_urls = []
@@ -32,8 +33,9 @@ def get_all_links(data_dict: dict):
                 and not any(href in full_url for href in hrefs_to_skip)
                 and "#" not in full_url
                 and not full_url.endswith(".com/")
+                and not full_url.endswith(".zip")
             ):
-                print(full_url)
+                # print(full_url)
                 try:
                     pdfkit.from_url(
                         full_url,
@@ -88,38 +90,88 @@ def main():
                 "/zh/",
                 "/zh/",
             ],
+            "pdf": False,
         },
         {
             "name": "SQLAlchemy",
             "base_url": "https://docs.sqlalchemy.org/en/20/",
             "hrefs_to_skip": [".zip"],
+            "pdf": False,
         },
         {
             "name": "Loguru",
             "base_url": "https://loguru.readthedocs.io/en/stable/",
             "hrefs_to_skip": ["/downloads/", "/0."],
+            "pdf": False,
         },
         {
             "name": "HTTPX",
             "base_url": "https://www.python-httpx.org/",
             "hrefs_to_skip": [],
+            "pdf": False,
         },
         {
             "name": "TQDM",
             "base_url": "https://tqdm.github.io/",
             "hrefs_to_skip": ["/slides", "/presentation", "/merch", "..", "video"],
+            "pdf": False,
         },
         {
             "name": "Pydantic",
             "base_url": "https://docs.pydantic.dev/latest/",
             "hrefs_to_skip": ["/dev/", "/1."],
+            "pdf": False,
         },
-        # {'name':'Xyz',"base_url": "https://xyz.com", "hrefs_to_skip": []},
-        # {'name':'Xyz',"base_url": "https://xyz.com", "hrefs_to_skip": []},
-        # {'name':'Xyz',"base_url": "https://xyz.com", "hrefs_to_skip": []},
+        # {
+        #     "name": "alembic",
+        #     "base_url": "https://alembic.sqlalchemy.org/en/latest/",
+        #     "hrefs_to_skip": [],
+        #     "pdf": False,
+        # },
+        {
+            "name": "Textblob",
+            "base_url": "https://textblob.readthedocs.io/_/downloads/en/latest/pdf/",
+            "hrefs_to_skip": [],
+            "pdf": True,
+        },
+        # {
+        #     "name": "MkDocs-Material",
+        #     "base_url": "https://squidfunk.github.io/mkdocs-material/",
+        #     "hrefs_to_skip": [],
+        #     "pdf": False,
+        # },
+        {
+            "name": "Pytest",
+            "base_url": "https://docs.pytest.org/_/downloads/en/7.4.x/pdf/",
+            "hrefs_to_skip": [],
+            "pdf": True,
+        },
+        # {
+        #     "name": "Passlib",
+        #     "base_url": "https://passlib.readthedocs.io/_/downloads/en/stable/pdf/",
+        #     "hrefs_to_skip": [],
+        #     "pdf": True,
+        # },
+        # {
+        #     "name": "Xyz",
+        #     "base_url": "https://xyz.com",
+        #     "hrefs_to_skip": [],
+        #     "pdf": False,
+        # },
     ]
     for library in tqdm(library_list, ascii=True, leave=True):
-        get_all_links(data_dict=library)
+        if library["pdf"] is True:
+            r = requests.get(library["base_url"], stream=True)
+            if r.status_code == 200:
+                with open(
+                    f'documents/{str(library["name"]).lower()}.pdf',
+                    "wb",
+                ) as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
+        else:
+            get_all_links(data_dict=library)
+            # pass
 
     merge_pdfs()
 
